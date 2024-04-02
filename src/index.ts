8c9ed7f693674f0560f2a8da24a56e3ce07f1ec5
Siddarth Koul <siddarthkoul21@gmail.com>
2024-04-02 15:13:47 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response }from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+import myUserRoute from "./routes/myUserRoute";
+
+mongoose
+.connect(process.env.MONGODB_CONNECTION_STRING as string)
+.then(()=>{console.log("Connected to Database ")}); 
+const app = express();
+app.use(express.json())
+app.use(cors())
+
+app.get("/health", async (req: Request, res: Response)=>{
+    res.send({ message: "health OK! "});
+});
+app.use("/api/my/user",myUserRoute);
+
+app.listen(8080,()=>{
+    console.log("Server is running on port 8080");
+})
